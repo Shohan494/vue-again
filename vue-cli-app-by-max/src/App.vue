620b9658bece9b878364fba860b2faf2c13fea5e
Shohan494 <shohan12224943@gmail.com>
2021-02-04 03:03:27 +0600

    preparing code for vue cli project

---
+++ vue-cli-app-by-max/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<section>
+		<h2>My Friends</h2>
+		<ul>
+			<li></li>
+		</ul>
+	</section>
+</template>
+
+<script>
+export default 
+{
+  data() {
+	return {
+		friends: [
+			{ id: 'shohan', name: 'Shohanul Alam', phone: '0188923974', email: 'shohan5917@gmail.com'},
+			{ id: 'abul', name: 'Shohanul Abul', phone: '0188923974', email: 'abul@gmail.com'}
+		]
+	}
+  },
+}
+</script>
