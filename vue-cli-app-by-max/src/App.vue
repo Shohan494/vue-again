68f3af2915edb6db0c8e05b078aee5eac2134068
Shohan494 <shohan12224943@gmail.com>
2021-02-04 04:28:53 +0600

    passing and using data through props

--- vue-cli-app-by-max/src/App.vue
+++ vue-cli-app-by-max/src/App.vue
@@ -2,8 +2,14 @@
 	<section>
 		<h2>My Friends</h2>
 		<ul>
-			<friend-contact></friend-contact>
-			<friend-contact></friend-contact>
+			<friend-contact 
+			name="Shohan" 
+			email-address="shohan5917@gmail.com">
+			</friend-contact>
+			<friend-contact 
+			name="Shohan" 
+			email-address="shohan5917@gmail.com">
+			</friend-contact>
 		</ul>
 	</section>
 </template>
