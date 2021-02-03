b42babb108dc16c139a89697bd58d4b999b0539b
Shohan494 <shohan12224943@gmail.com>
2021-02-04 03:45:57 +0600

    prepared friend contact vue component

---
+++ vue-cli-app-by-max/src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+	<li>
+		<h2>
+			{{ friend.name }}
+		</h2>
+		<button @click="toggleDetails">
+			Show Details
+		</button>
+		<ul v-if="detailsAreAvailable">
+			<li><strong>Phone: </strong>{{ friend.phone }}</li>
+			<li><strong>Email: </strong>{{ friend.email }}</li>
+		</ul>
+	</li>
+</template>
+
+<script>
+export default {
+	data() {
+		detailsAreAvailable: false,
+		friend: { 
+			id: 'shohan', 
+			name: 'Shohanul Alam', 
+			phone: '0188923974', 
+			email: 'shohan5917@gmail.com'
+		}
+	},
+  methods: {
+    toggleDetails(){
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+}
+</script>	
+
+
