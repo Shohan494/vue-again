0b27deb231578dcf5608ce2ab31354596a90ec46
Shohan494 <shohan12224943@gmail.com>
2021-02-05 02:28:15 +0600

    sending data worked, from chrome

--- http-request-vue/src/components/survey/LearningSurvey.vue
+++ http-request-vue/src/components/survey/LearningSurvey.vue
@@ -46,7 +46,7 @@ export default {
       invalidInput: false,
     };
   },
-  emits: ['survey-submit'],
+  //emits: ['survey-submit'],
   methods: {
     submitSurvey() {
       if (this.enteredName === '' || !this.chosenRating) {
@@ -55,10 +55,10 @@ export default {
       }
       this.invalidInput = false;
 
-      this.$emit('survey-submit', {
-        userName: this.enteredName,
-        rating: this.chosenRating,
-      });
+      // this.$emit('survey-submit', {
+      //   userName: this.enteredName,
+      //   rating: this.chosenRating,
+      // });
 
       fetch('https://vue-http-50d86-default-rtdb.firebaseio.com/surveys.json', {
         method: 'POST',
