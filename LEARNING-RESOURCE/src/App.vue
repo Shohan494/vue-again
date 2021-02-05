9812bb503867349ef7d85ea6d3faf16c23312ae3
Shohan494 <shohan12224943@gmail.com>
2021-02-06 00:37:28 +0600

    first we need the main.js, then the src/App.vue, then we are adding the stored resource in data

---
+++ LEARNING-RESOURCE/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<ul>
+		
+	</ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    	storedResources: [
+    		{ 
+    			id: '1', 
+    			title: 'Official Guide', 
+    			description: 'The official vuejs documentation', 
+    			link: 'https://vuejs.org' 
+    		},
+    		{ 
+    			id: '2', 
+    			title: 'Google', 
+    			description: 'The biggest resource of learning stuffs', 
+    			link: 'https://google.com' 
+    		}
+    	]
+  },
+  provide() {
+
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  }
+};
+</script>
