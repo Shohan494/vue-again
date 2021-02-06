7b3ffc18355d51f7e0f45eb06d16248fa57198c1
Shohan494 <shohan12224943@gmail.com>
2021-02-06 17:16:49 +0600

    main and using two components, passing data by using props and binding them

---
+++ LEARNING-RESOURCE/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="resource in storedResources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+export default {
+  props: ['storedResources'],
+  components: {
+    LearningResource
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
