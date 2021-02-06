792810bcae12a096e7902d6728a6be0c3f7dc0ff
Shohan494 <shohan12224943@gmail.com>
2021-02-06 14:18:20 +0600

    resource components creation basic

--- LEARNING-RESOURCE/src/App.vue
+++ LEARNING-RESOURCE/src/App.vue
@@ -1,36 +1,34 @@
 <template>
-	<ul>
-		
-	</ul>
+  <ul>
+    <li v-for="resource in storedResources" :key="resource.id">
+      {{ resource.title }}
+    </li>
+  </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
 export default {
+  components: {
+    LearningResource
+  },
   data() {
     return {
-    	storedResources: [
-    		{ 
-    			id: '1', 
-    			title: 'Official Guide', 
-    			description: 'The official vuejs documentation', 
-    			link: 'https://vuejs.org' 
-    		},
-    		{ 
-    			id: '2', 
-    			title: 'Google', 
-    			description: 'The biggest resource of learning stuffs', 
-    			link: 'https://google.com' 
-    		}
-    	]
+      storedResources: [
+        {
+          id: '1',
+          title: 'Official Guide',
+          description: 'The official vuejs documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: '2',
+          title: 'Google',
+          description: 'The biggest resource of learning stuffs',
+          link: 'https://google.com',
+        },
+      ],
+    };
   },
-  provide() {
-
-  },
-  methods: {
-
-  },
-  mounted() {
-
-  }
 };
 </script>
