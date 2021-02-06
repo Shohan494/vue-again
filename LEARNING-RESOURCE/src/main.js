88e5df4b1f8d4f4836671aeda3348eae745f4aa7
Shohan494 <shohan12224943@gmail.com>
2021-02-06 17:38:54 +0600

    adding ui base card global component and registering in main.js

--- LEARNING-RESOURCE/src/main.js
+++ LEARNING-RESOURCE/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue';
+
 import App from './App.vue';
+import BaseCard from './components/UI/BaseCard.vue';
+// GLOBAL COMPONENT REGISTRATION
 
+const app = createApp(App);
 
-createApp(App).mount('#app');
+app.component('base-card', BaseCard);
+
+app.mount('#app');
