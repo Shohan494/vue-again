dee2e1b3bf6c5c088a7de3fb187a4c00b93ff16e
Shohan494 <shohan12224943@gmail.com>
2021-02-06 19:52:15 +0600

    adding header component

--- LEARNING-RESOURCE/src/App.vue
+++ LEARNING-RESOURCE/src/App.vue
@@ -1,11 +1,15 @@
 <template>
+  <the-header :title="headerName"></the-header>
   <stored-resources :storedResources="storedResources"></stored-resources>
 </template>
 
 <script>
 import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+
 export default {
   components: {
+    TheHeader,
     StoredResources,
   },
   data() {
@@ -24,6 +28,7 @@ export default {
           link: 'https://google.com',
         },
       ],
+      headerName: 'Vue Learning Resource'
     };
   },
 };
