e48fb66011862aa27aa3614162a05b34d691ed31
Shohan494 <shohan12224943@gmail.com>
2021-02-17 22:30:33 +0600

    vuex basic, store, state

--- vuex-project/src/main.js
+++ vuex-project/src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
 import App from './App.vue';
 
 const app = createApp(App);
 
+const store = createStore({
+    state(){
+        return {
+            counter: 0
+        };
+    }
+});
+
 app.mount('#app');
+
+app.use(store);
